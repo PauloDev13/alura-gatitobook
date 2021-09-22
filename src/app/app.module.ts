0307813dc04285cf51161ac79a5a6ec63815e825
PauloDev13 <devpgm2020@gmail.com>
2021-09-22 16:21:16 -0300

    Implementing Home Page Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeRoutingModule } from './home/home-routing.module';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeRoutingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, HomeRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
