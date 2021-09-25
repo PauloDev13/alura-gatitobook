dd9414222a6320d53fdee6d169238bf5ac1d31d5
PauloDev13 <devpgm2020@gmail.com>
2021-09-25 17:57:01 -0300

    Implementing preview on image upload

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-novo-animal',
   templateUrl: './novo-animal.component.html',
-  styleUrls: ['./novo-animal.component.css']
+  styleUrls: ['./novo-animal.component.css'],
 })
 export class NovoAnimalComponent implements OnInit {
+  formularioAnimal!: FormGroup;
+  file: File = new File([], '');
+  preview = '';
+  percentualConcluido = 0;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formularioAnimal = this.formBuilder.group({});
   }
 
+  gravaArquivo(arquivo: EventTarget) {}
+
+  upload() {}
 }
