3b4aa3ef2fee3da9786324b142d4b27a9fcb3bd8
PauloDev13 <devpgm2020@gmail.com>
2021-09-25 01:04:37 -0300

    Implementing Logic in the Comments Component

--- src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ComentarioService } from './comentario.service';
+import { Observable, of } from 'rxjs';
+import { Comentarios } from './comentario.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-comentarios',
   templateUrl: './comentarios.component.html',
-  styleUrls: ['./comentarios.component.css']
+  styleUrls: ['./comentarios.component.css'],
 })
 export class ComentariosComponent implements OnInit {
+  @Input()
+  animalId: number = 0;
+  comentarios$: Observable<Comentarios> = of([]);
+  comentarioForm: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private comentarioService: ComentarioService,
+  ) {}
 
   ngOnInit(): void {
+    this.comentarios$ = this.comentarioService.buscarComentarios(this.animalId);
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    });
   }
 
+  gravar(): void {
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentarioService
+      .incluirComentario(this.animalId, comentario)
+      .pipe(
+        switchMap(() =>
+          this.comentarioService.buscarComentarios(this.animalId),
+        ),
+        tap(() => {
+          this.comentarioForm.reset();
+          alert('Comentário salvo com sucesso');
+        }),
+      );
+  }
 }
