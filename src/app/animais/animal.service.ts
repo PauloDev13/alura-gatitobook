82af81460d3318df5742d79190bbd51444068ca6
PauloDev13 <devpgm2020@gmail.com>
2021-09-25 00:16:09 -0300

    Implementing Like and Exclude Animals Methods

--- src/app/animais/animal.service.ts
+++ src/app/animais/animal.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Animais, Animal } from './animal.interface';
 import { environment } from '../../environments/environment';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.baseUrl;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimalService {
   constructor(
-    private httpClient: HttpClient,
-  ) // private tokenService: TokenService,
-  {}
+    private httpClient: HttpClient, // private tokenService: TokenService,
+  ) {}
 
   listaDeUsuario(nomeDoUsuario: string): Observable<Animais> {
     // Foi substituído pelo AutenticacaoInterceptor
@@ -31,5 +32,26 @@ export class AnimalService {
     return this.httpClient.get<Animal>(`${API}/photos/${id}`, {
       // headers,
     });
+  }
+
+  excluirAnimal(id: number): Observable<Animal> {
+    return this.httpClient.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.httpClient
+      .post(
+        `${API}/photos/${id}/like`,
+        {},
+        {
+          observe: 'response',
+        },
+      )
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        }),
+      );
   }
 }
--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Animal } from '../animal.interface';
 import { Observable, of } from 'rxjs';
 import { AnimalService } from '../animal.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
@@ -10,16 +10,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detalhe-animal.component.css'],
 })
 export class DetalheAnimalComponent implements OnInit {
-  // animalId: number = 0;
+  animalId: number = 0;
   animal$: Observable<Animal> = of({} as Animal);
 
   constructor(
     private animalService: AnimalService,
     private activatedRoute: ActivatedRoute,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
-    const id = this.activatedRoute.snapshot.params.animalId;
-    this.animal$ = this.animalService.buscaPorId(id);
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animalService.buscaPorId(this.animalId);
+  }
+
+  curtir() {
+    this.animalService.curtir(this.animalId).subscribe((curtida) => {
+      if (curtida) {
+        this.animal$ = this.animalService.buscaPorId(this.animalId);
+      }
+    });
+  }
+
+  excluir() {
+    this.animalService.excluirAnimal(this.animalId).subscribe(
+      () => this.router.navigate(['/animais']),
+      (error) => console.error(error),
+    );
   }
 }
