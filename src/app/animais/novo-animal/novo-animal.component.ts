a2ebb6106e01288862da745ba0553be49ab83ad4
PauloDev13 <devpgm2020@gmail.com>
2021-09-25 18:36:22 -0300

    Implementing New Animal Component Logic

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AnimalService } from '../animal.service';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-novo-animal',
@@ -12,13 +16,44 @@ export class NovoAnimalComponent implements OnInit {
   preview = '';
   percentualConcluido = 0;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private animalService: AnimalService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
-    this.formularioAnimal = this.formBuilder.group({});
+    this.formularioAnimal = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
 
-  gravaArquivo(arquivo: EventTarget | null) {}
+  gravaArquivo(arquivo: any) {
+    const [file] = arquivo?.files;
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => (this.preview = event.target.result);
+    reader.readAsDataURL(file);
+  }
 
-  upload() {}
+  upload() {
+    const allowComments =
+      this.formularioAnimal.get('allowComments')?.value ?? false;
+    const description = this.formularioAnimal.get('description')?.value ?? '';
+
+    this.animalService
+      .upload(description, allowComments, this.file)
+      .pipe(finalize(() => this.router.navigate(['animais'])))
+      .subscribe(
+        (event: HttpEvent<any>) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            const total = event.total ?? 1;
+            this.percentualConcluido = Math.round(100 * (event.loaded / total));
+          }
+        },
+        (error) => console.error(error),
+      );
+  }
 }
